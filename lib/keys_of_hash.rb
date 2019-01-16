f2f349b24667d872147a45ef1b357b13fa1c4832
jamesdorr3 <jamesdorr3@gmail.com>
2019-01-15 18:41:54 -0800

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*argsarray) #self is a hash
+    temparray = []
+    argsarray.each do |iteminarray|
+      self.each do |iteminhash|
+        if iteminhash[1] == iteminarray
+          temparray << iteminhash[0]
+        end
+      end
+    end
+    temparray
   end
 end
